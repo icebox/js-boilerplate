e7054c3707f17edbabff802d5e55e14af4b6c023
Jonathan Schmidt <80654+icebox@users.noreply.github.com>
2022-06-17 13:49:33 -0700

    babel-loader config

--- webpack.config.js
+++ webpack.config.js
@@ -40,9 +40,13 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: [
-          "babel-loader"
-        ]
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
       },
       {
         test: /\.css$/,
